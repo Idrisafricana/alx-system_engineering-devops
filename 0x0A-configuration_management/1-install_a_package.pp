912f5da6de9fc011500a4a7b24c50c500ce5158a
idrisafricana <adeleyedml2@gmail.com>
2024-05-04 23:40:23 +0100

    Changes to version on the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet -v 2.1.0
+# install puppet -v 2.5.0
 
 exec { 'puppet':
-  command => '/usr/bin/apt-get -y install puppet -v 2.1.0',
+  command => '/usr/bin/apt-get -y install puppet -v 2.5.0',
 }
