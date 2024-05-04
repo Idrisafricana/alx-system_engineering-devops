e69ed66b90fb2a46df85f3dc700bf7746bb6bfdd
idrisafricana <adeleyedml2@gmail.com>
2024-05-04 23:43:25 +0100

    Changes to version on the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet -v 2.5.0
+# install puppet-lint -v 2.5.0
 
-exec { 'puppet':
-  command => '/usr/bin/apt-get -y install puppet -v 2.5.0',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
